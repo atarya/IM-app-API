33eae79d2800a7bf36a7396b6f07b6ad6178a77b
Anupam Arya <anu9am@gmail.com>
2022-03-08 02:03:48 +0530

    fe axios w server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const app = express();
 const dotenv = require("dotenv");
 const PORT = process.env.PORT || 3000
 dotenv.config({ path: "./../.env" });
+const cors = require('cors');
+app.use(cors({
+    origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 
 app.get("/", (req, res) => {
     res.send("API is working");
