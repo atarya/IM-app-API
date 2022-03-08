8316eaf6aec3eb9364326f573ca5f0599bbeab83
Anupam Arya <anu9am@gmail.com>
2022-03-08 10:42:48 +0530

    chat routes

--- server/server.js
+++ server/server.js
@@ -19,17 +19,18 @@ app.get("/", (req, res) => {
     res.send("API is working");
 })
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats);
-})
+// app.get("/api/chat", (req, res) => {
+//     res.send(chats);
+// })
 
-app.get("/api/chat/:id", (req, res) => {
-    // console.log(req.params.id)
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat)
-})
+// app.get("/api/chat/:id", (req, res) => {
+//     // console.log(req.params.id)
+//     const singleChat = chats.find((c) => c._id === req.params.id);
+//     res.send(singleChat)
+// })
 
-app.use('/api/user', require('./routes/user'));
+app.use('/api/user', require('./Routes/user'));
+app.use('/api/chat', require('./Routes/chat'));
 
 app.use(notFound);
 app.use(errorHandler);
