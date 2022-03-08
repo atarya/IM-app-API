4d81f81fa5f5348eeebb74080838d00b5ecc87f6
Anupam Arya <anu9am@gmail.com>
2022-03-08 11:08:32 +0530

    chat controllers

--- server/Models/Chat.js
+++ server/Models/Chat.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
-    chatName: { type: String, trim=true },
+    chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
